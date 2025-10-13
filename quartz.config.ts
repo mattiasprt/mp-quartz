5a057e807b7a988309f22a6b4c737fe2f698a044
mattiasprt <mattiasprt@gmail.com>
2025-10-13 23:01:32 +0300

    Quartz sync: Oct 13, 2025, 11:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
-    locale: "et-EE",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
