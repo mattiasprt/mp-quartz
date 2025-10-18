8d6b66c57b73176546eb243d215e59a1a3d31ca3
mattiasprt <mattiasprt@gmail.com>
2025-10-18 22:58:28 +0300

    Quartz sync: Oct 18, 2025, 10:58 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -62,7 +62,7 @@ export const defaultListPageLayout: PageLayout = {
           Component: Component.Search(),
           grow: true,
         },
-        { Component: Component.Darkmode() },
+        /* { Component: Component.Darkmode() }, */
       ],
     }),
     Component.Explorer({  
