0b37f5c1c5b7b47f8c8cbc69ab70bad842c9b4c0
mattiasprt <mattiasprt@gmail.com>
2026-03-24 17:53:07 +0200

    Quartz sync: Mar 24, 2026, 5:53 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,17 +35,17 @@ export const defaultContentPageLayout: PageLayout = {
         /* { Component: Component.ReaderMode() }, */
       ],
     }),
-    Component.Explorer({  
-      mapFn: (node) => {  
-        if (!node.isFolder) {  
-          node.displayName = "• " + node.displayName  
-        }  
-      },  
+    Component.Explorer({
+      mapFn: (node) => {
+        if (!node.isFolder) {
+          node.displayName = "• " + node.displayName
+        }
+      },
     }),
   ],
   right: [
     /* Component.Graph(), */
-    /* Component.DesktopOnly(Component.TableOfContents()), */
+    Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
@@ -65,12 +65,12 @@ export const defaultListPageLayout: PageLayout = {
         /* { Component: Component.Darkmode() }, */
       ],
     }),
-    Component.Explorer({  
-      mapFn: (node) => {  
-        if (!node.isFolder) {  
-          node.displayName = "• " + node.displayName  
-        }  
-      },  
+    Component.Explorer({
+      mapFn: (node) => {
+        if (!node.isFolder) {
+          node.displayName = "• " + node.displayName
+        }
+      },
     }),
   ],
   right: [],
